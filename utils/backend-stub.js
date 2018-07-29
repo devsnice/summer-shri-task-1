207954313d9c0479c23f347f6316796dab901b25
Borisov Semen <devsnice@gmail.com>
2018-07-29 16:38:59 +0300

    prettier format files

--- utils/backend-stub.js
+++ utils/backend-stub.js
@@ -1,20 +1,18 @@
-const { generateData, generateDetails } = require('./generate-data');
+const { generateData, generateDetails } = require("./generate-data");
 
 const data = generateData();
 const cache = {};
 
 exports.initBackendStub = function(app) {
-  app.get('/api/stations', (req, res) => {
-    res.json(data);
-  });
+	app.get("/api/stations", (req, res) => {
+		res.json(data);
+	});
 
-  app.get('/api/stations/:id', (req, res) => {
-    const id = req.params.id;
-    const info = data[id];
-    const details = cache[id] || (cache[id] = generateDetails(info));
+	app.get("/api/stations/:id", (req, res) => {
+		const id = req.params.id;
+		const info = data[id];
+		const details = cache[id] || (cache[id] = generateDetails(info));
 
-    res.json({ ...info, ...details });
-
-    return {};
-  });
+		return res.json({ ...info, ...details });
+	});
 };
