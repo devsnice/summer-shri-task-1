78f4701ee216bc9b607838163cfc59d1e5676ab5
Borisov Semen <devsnice@gmail.com>
2018-08-04 11:45:42 +0300

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	extends: ["google"],
+	parserOptions: {
+		sourceType: "module"
+	},
+	env: {
+		browser: true
+	},
+	rules: {
+		"require-jsdoc": 0,
+		"no-tabs": 0,
+		"object-curly-spacing": 0,
+		"comma-dangle": 0,
+		quotes: 0
+	}
+};
